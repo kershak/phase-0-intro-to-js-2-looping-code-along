6155a91eaf22ea719b04c29d45dc79ac9d3de53c
NillsLopez <yakusaz17@gmail.com>
2024-08-15 21:12:09 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(nameProvided, event){
+    let thankYouMessages = [] ;
+        for ( let i = 0; i < nameProvided.length; i++ ) {
+            thankYouMessages.push( `Thank you, ${nameProvided[i]}, for the wonderful ${event} gift!` );
+        }   
+    return thankYouMessages;  
+}
+
+
+function countDown(firstNumber){   
+    while ( firstNumber > 0 ) { 
+        console.log( firstNumber);  
+        firstNumber -= 1 ;
+    }
+    console.log ( firstNumber);
+}               
